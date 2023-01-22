c9895c432209484468985fc1d88433b80e31167d
Nataniel <natuss.m@~gmail.com>
2023-01-22 10:15:32 -0300

    Entrega e complemento do exercicio

--- src/App.java
+++ src/App.java
@@ -1,11 +1,23 @@
 import edu.nataniel.exercicios.Carro;
-
+/**
+ * Exercicio de exemplo sobre Classes e Objetos
+ */
 public class App {
+    /**
+     * Construtor pode ser vazio ou com sobrecarga;
+     * 
+     */
     public static void main(String[] args) throws Exception {
-        Carro meuCarro = new Carro(45, "Renolte");
+        Carro meuCarro = new Carro("Renault", "Clio", "Amarelo", "Manual", 4, 40, 2001, 80);
         //meuCarro.setMarca("Renolt");
         System.out.println(meuCarro.getMarca());
-        //meuCarro.setCapacidadeTanque(45);
+        System.out.println(meuCarro.getModelo());
+        System.out.println(meuCarro.getCor());
+        System.out.println(meuCarro.getCambio());
+        System.out.println(meuCarro.getPortas() + " portas");
+        System.out.println(meuCarro.getCapacidadeTanque() + " litros");
+        System.out.println(meuCarro.getPotencia()+ " CV");
+        System.out.println(meuCarro.getAnoFabricacao() + " Ano de fabricação");
 
         System.out.println("O valor para encher o tanque do carro é: " + meuCarro.valorEncherTanque(4.79));
     }
--- src/edu/nataniel/exercicios/Carro.java
+++ src/edu/nataniel/exercicios/Carro.java
@@ -9,21 +9,27 @@ public class Carro{
 
     //Atributos
     private String marca;
-    String modelo;
-    String cor;
-    String cambio;
-    int portas;
+    private String modelo;
+    private String cor;
+    private String cambio;
+    private int portas;
     private int capacidadeTanque;
-    int anoFabricacao;
-    int potencia;
+    private int anoFabricacao;
+    private int potencia;
 
     //Construtor - provê valores iniciais
     public Carro(){
     }
     //Sobrecarga 1
-    public Carro(int capacidadeTanque, String marca){
+    public Carro(String marca, String modelo, String cor, String cambio, int portas, int capacidadeTanque, int anoFabricacao, int potencia){
         this.capacidadeTanque = capacidadeTanque;
+        this.modelo = modelo;
         this.marca = marca;
+        this.cor = cor;
+        this.cambio = cambio;
+        this.portas = portas;
+        this.anoFabricacao = anoFabricacao;
+        this.potencia = potencia;
     }
 
 
@@ -32,6 +38,48 @@ public class Carro{
     }
     public String getMarca(){
         return this.marca;
+    }
+
+    public void setCor(String cor){
+        this.cor = cor;
+    }
+    public String getCor(){
+        return this.cor;
+    }
+
+    public void setCambio(String cambio){
+        this.cambio = cambio;
+    }
+    public String getCambio(){
+        return this.cambio;
+    }
+    public void setPortas(int portas){
+        this.portas = portas;
+    }
+    public int getPortas(){
+        return this.portas;
+    }
+
+    public void setAnoFabricacao(int ano){
+        this.anoFabricacao = ano;
+    }
+    public int getAnoFabricacao(){
+        return this.anoFabricacao;
+    }
+
+    public void setPotencia(int potencia){
+        this.potencia = potencia;
+    }
+    public int getPotencia(){
+        return this.potencia;
+    }
+
+
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+    public String getModelo(){
+        return this.modelo;
     }
 
     public void setCapacidadeTanque(int litros){
